e5cab3d5707957d7fbfceaba9cf88b7273835c9d
phwise <phmkize@gmail.com>
2023-12-13 11:25:54 +0200

    initial commits

---
+++ src/main/java/com/wisefoodsApi/WisefoodsApiApplication.java
@@ -0,0 +1,13 @@
+package com.wisefoodsApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WisefoodsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WisefoodsApiApplication.class, args);
+	}
+
+}
